a75778b659766c81c616fc9016ebf5e40cb367d0
Dawson.Gao <dawson.gao@libratone.com.cn>
2019-02-18 18:07:50 +0800

    init Jobscheduler, and BackgroundTaskManager
    init Jobscheduler, and BackgroundTaskManager

---
+++ app/src/main/java/dawson/com/backgrounddialog/BackGroundTaskManager.java
@@ -0,0 +1,120 @@
+package dawson.com.backgrounddialog;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Messenger;
+import android.os.PersistableBundle;
+import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static android.content.ContentValues.TAG;
+
+public class BackGroundTaskManager {
+    private int mJobId;
+    private ComponentName componentName;
+    private Context context;
+    public static final String MESSENGER_INTENT_KEY = BuildConfig.APPLICATION_ID + ".MESSENGER_INTENT_KEY";
+    public static final String WORK_DURATION_KEY = BuildConfig.APPLICATION_ID + ".WORK_DURATION_KEY";
+    private Handler handler;
+
+    private BackGroundTaskManager() {
+    }
+
+    public static BackGroundTaskManager getInstance() {
+        return Holder.instance;
+    }
+
+    private static class Holder {
+        public static final BackGroundTaskManager instance = new BackGroundTaskManager();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void init(Context context, Handler handler) {
+        this.context = context;
+        this.handler = handler;
+        componentName = new ComponentName(context, MyJobService.class);
+    }
+
+    public void start() {
+        // Start service and provide it a way to communicate with this class.
+        Intent startServiceIntent = new Intent(context, MyJobService.class);
+        Messenger messengerIncoming = new Messenger(handler);
+        startServiceIntent.putExtra(MESSENGER_INTENT_KEY, messengerIncoming);
+        context.startService(startServiceIntent);
+    }
+
+    public void stop() {
+        // A service can be "started" and/or "bound". In this case, it's "started" by this Activity
+        // and "bound" to the JobScheduler (also called "Scheduled" by the JobScheduler). This call
+        // to stopService() won't prevent scheduled jobs to be processed. However, failing
+        // to call stopService() would keep it alive indefinitely.
+        context.stopService(new Intent(context, MyJobService.class));
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void cancelAllJobs() {
+        JobScheduler tm = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        tm.cancelAll();
+        Toast.makeText(context, R.string.all_jobs_cancelled, Toast.LENGTH_SHORT).show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void scheduleJob(long minLatencyMillis,
+                            long maxExecutionDelayMillis,
+                            boolean requiresUnmetered,
+                            boolean requiresAnyConnectivity,
+                            boolean requiresIdleCheckboxChecked,
+                            boolean requiresChargingCheckBoxChecked,
+                            String workDuration) {
+        JobInfo.Builder builder = new JobInfo.Builder(mJobId++, componentName);
+
+        // Extras, work duration.
+        PersistableBundle extras = new PersistableBundle();
+        if (TextUtils.isEmpty(workDuration)) {
+            workDuration = "1";
+        }
+        extras.putLong(WORK_DURATION_KEY, Long.valueOf(workDuration) * 1000);
+        builder.setExtras(extras);
+
+        // Schedule job
+        Log.d(TAG, "Scheduling job");
+        builder.setMinimumLatency(minLatencyMillis);
+        builder.setOverrideDeadline(maxExecutionDelayMillis);
+        if (requiresUnmetered) {
+            builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED);
+        } else if (requiresAnyConnectivity) {
+            builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
+        }
+        builder.setRequiresDeviceIdle(requiresIdleCheckboxChecked);
+        builder.setRequiresCharging(requiresChargingCheckBoxChecked);
+        JobScheduler tm = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        tm.schedule(builder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void cancelFirstJob() {
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        List<JobInfo> allPendingJobs = jobScheduler.getAllPendingJobs();
+        if (allPendingJobs.size() > 0) {
+            // Finish the last one
+            int jobId = allPendingJobs.get(0).getId();
+            jobScheduler.cancel(jobId);
+            Toast.makeText(
+                    context, String.format(context.getString(R.string.cancelled_job), jobId),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(
+                    context, context.getString(R.string.no_jobs_to_cancel),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/dawson/com/backgrounddialog/MainActivity.java
+++ app/src/main/java/dawson/com/backgrounddialog/MainActivity.java
@@ -1,41 +1,203 @@
 package dawson.com.backgrounddialog;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.PersistableBundle;
+import android.support.annotation.ColorRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.ref.WeakReference;
 
+import static dawson.com.backgrounddialog.BackGroundTaskManager.WORK_DURATION_KEY;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int MSG_UNCOLOR_START = 0;
+    public static final int MSG_UNCOLOR_STOP = 1;
+    public static final int MSG_COLOR_START = 2;
+    public static final int MSG_COLOR_STOP = 3;
+
+
+    private EditText mDelayEditText;
+    private EditText mDeadlineEditText;
+    private EditText mDurationTimeEditText;
+    private RadioButton mWiFiConnectivityRadioButton;
+    private RadioButton mAnyConnectivityRadioButton;
+    private CheckBox mRequiresChargingCheckBox;
+    private CheckBox mRequiresIdleCheckbox;
+
+
+    private int mJobId = 0;
+
+    // Handler for incoming messages from the service.
+    private IncomingMessageHandler mHandler;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.sample_main);
+
+        // Set up UI.
+        mDelayEditText = findViewById(R.id.delay_time);
+        mDurationTimeEditText = findViewById(R.id.duration_time);
+        mDeadlineEditText = findViewById(R.id.deadline_time);
+        mWiFiConnectivityRadioButton = findViewById(R.id.checkbox_unmetered);
+        mAnyConnectivityRadioButton = findViewById(R.id.checkbox_any);
+        mRequiresChargingCheckBox = findViewById(R.id.checkbox_charging);
+        mRequiresIdleCheckbox = findViewById(R.id.checkbox_idle);
+
+        mHandler = new IncomingMessageHandler(this);
+
+        BackGroundTaskManager.getInstance().init(this, mHandler);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onStop() {
+        super.onStop();
+        BackGroundTaskManager.getInstance().stop();
+    }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-                builder.setMessage("Hello,My name is Message.");
-                builder.setNegativeButton("CANCEL", null);
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        // 具体操作
-                    }
-                });
-                final Dialog dialog = builder.create();
-                dialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-                dialog.show();
+    @Override
+    protected void onStart() {
+        super.onStart();
+        BackGroundTaskManager.getInstance().start();
+    }
+
+    /**
+     * Executed when user clicks on SCHEDULE JOB.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void scheduleJob(View v) {
+        String delay = mDelayEditText.getText().toString();
+        long minLatencyMillis = 0;
+        if (!TextUtils.isEmpty(delay)) {
+            minLatencyMillis = Long.valueOf(delay) * 1000;
+        }
+        String deadline = mDeadlineEditText.getText().toString();
+        long maxExecutionDelayMillis = 0;
+        if (!TextUtils.isEmpty(deadline)) {
+            maxExecutionDelayMillis = Long.valueOf(deadline) * 1000;
+        }
+        boolean requiresUnmetered = mWiFiConnectivityRadioButton.isChecked();
+        boolean requiresAnyConnectivity = mAnyConnectivityRadioButton.isChecked();
+        boolean requiresIdleCheckboxChecked = mRequiresIdleCheckbox.isChecked();
+        boolean requiresChargingCheckBoxChecked = mRequiresChargingCheckBox.isChecked();
+
+        String workDuration = mDurationTimeEditText.getText().toString();
+
+        BackGroundTaskManager.getInstance().scheduleJob(minLatencyMillis,
+                maxExecutionDelayMillis, requiresUnmetered, requiresAnyConnectivity,
+                requiresIdleCheckboxChecked, requiresChargingCheckBoxChecked, workDuration);
+    }
+
+    /**
+     * Executed when user clicks on CANCEL ALL.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void cancelAllJobs(View v) {
+        BackGroundTaskManager.getInstance().cancelAllJobs();
+    }
+
+    /**
+     * Executed when user clicks on FINISH LAST TASK.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public void finishJob(View v) {
+        BackGroundTaskManager.getInstance().cancelFirstJob();
+    }
+
+    /**
+     * A {@link Handler} allows you to send messages associated with a thread. A {@link Messenger}
+     * uses this handler to communicate from {@link MyJobService}. It's also used to make
+     * the start and stop views blink for a short period of time.
+     */
+    public static class IncomingMessageHandler extends Handler {
+
+        // Prevent possible leaks with a weak reference.
+        private WeakReference<MainActivity> mActivity;
+
+        IncomingMessageHandler(MainActivity activity) {
+            super(/* default looper */);
+            this.mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MainActivity mainActivity = mActivity.get();
+            if (mainActivity == null) {
+                // Activity is no longer available, exit.
+                return;
             }
-        }, 500);
+            View showStartView = mainActivity.findViewById(R.id.onstart_textview);
+            View showStopView = mainActivity.findViewById(R.id.onstop_textview);
+            Message m;
+            switch (msg.what) {
+                /*
+                 * Receives callback from the service when a job has landed
+                 * on the app. Turns on indicator and sends a message to turn it off after
+                 * a second.
+                 */
+                case MSG_COLOR_START:
+                    // Start received, turn on the indicator and show text.
+                    showStartView.setBackgroundColor(getColor(R.color.start_received));
+                    updateParamsTextView(msg.obj, "started");
+
+                    // Send message to turn it off after a second.
+                    m = Message.obtain(this, MSG_UNCOLOR_START);
+                    sendMessageDelayed(m, 1000L);
+                    break;
+                /*
+                 * Receives callback from the service when a job that previously landed on the
+                 * app must stop executing. Turns on indicator and sends a message to turn it
+                 * off after two seconds.
+                 */
+                case MSG_COLOR_STOP:
+                    // Stop received, turn on the indicator and show text.
+                    showStopView.setBackgroundColor(getColor(R.color.stop_received));
+                    updateParamsTextView(msg.obj, "stopped");
+
+                    // Send message to turn it off after a second.
+                    m = obtainMessage(MSG_UNCOLOR_STOP);
+                    sendMessageDelayed(m, 2000L);
+                    break;
+                case MSG_UNCOLOR_START:
+                    showStartView.setBackgroundColor(getColor(R.color.none_received));
+                    updateParamsTextView(null, "");
+                    break;
+                case MSG_UNCOLOR_STOP:
+                    showStopView.setBackgroundColor(getColor(R.color.none_received));
+                    updateParamsTextView(null, "");
+                    break;
+            }
+        }
+
+        private void updateParamsTextView(@Nullable Object jobId, String action) {
+            TextView paramsTextView = mActivity.get().findViewById(R.id.task_params);
+            if (jobId == null) {
+                paramsTextView.setText("");
+                return;
+            }
+            String jobIdText = String.valueOf(jobId);
+            paramsTextView.setText(String.format("Job ID %s %s", jobIdText, action));
+        }
+
+        private int getColor(@ColorRes int color) {
+            return mActivity.get().getResources().getColor(color);
+        }
     }
 }
+
